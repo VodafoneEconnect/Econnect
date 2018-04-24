09109b3f5dc084ce5c6938b0fb6dada529a467f4
Hemant Faujdar <>
2018-04-24 15:04:10 +0530

    fix

--- scripts/mobile-scripts.js
+++ scripts/mobile-scripts.js
@@ -6,19 +6,44 @@ function hideURLbar(){ window.scrollTo(0,1); }
 /* JQUERY */
 
 $(document).ready(function() {
+    var isMobile = {
+        Windows: function () {
+            return /IEMobile/i.test(navigator.userAgent);
+        },
+        Android: function () {
+            return /Android/i.test(navigator.userAgent);
+        },
+        BlackBerry: function () {
+            return /BlackBerry/i.test(navigator.userAgent);
+        },
+        iOS: function () {
+            return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+        },
+        any: function () {
+            return !(isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+        }
+    };
+    
+
     if(isMobile.Android()){
-        $("#appSize").html("4.7 MB");
+        
+        $("#appSize").html(versionInfo.ANDROID.Size);
+        $("#appVersion").html(versionInfo.ANDROID.Version);
+        $("#appLastUpdate").html(versionInfo.ANDROID.LastUpdateDate);
+        
     }
     else{
-        $("#appSize").html("11.6 MB")
+        $("#appSize").html(versionInfo.IOS.Size);
+        $("#appVersion").html(versionInfo.IOS.Version);
+        $("#appLastUpdate").html(versionInfo.IOS.LastUpdateDate);
     }
     // Comment Toggle
     $('#btnInstall').click(function () {
         if(isMobile.Android()){
-            window.location.href = "./builds/econnect-release-beta-v1.0.0.10002.apk";
+            window.location.href = "./builds/Econnect.apk";
         }
         else{
-            window.location.href = "itms-services://?action=download-manifest&amp;url=https://vodafoneeconnect.github.io/Econnect/builds/app.plist";
+            window.location.href = "itms-services://?action=download-manifest&amp;url=./builds/app.plist";
         }
         
     });
